a7f96fba890b5a622b0683bef594ecfc837b74ff
Jim Pisano <jpisano@cisco.com>
2019-01-11 12:07:18 -0500

    jim's work laptop

--- build_customer_list.py
+++ build_customer_list.py
@@ -10,7 +10,7 @@ def build_customer_list():
     #
     home = app['HOME']
     working_dir = app['WORKING_DIR']
-    bookings_file = app['BOOKINGS'] # Master bookings has 9958 rows as of 12-15-18
+    bookings_file = app['BOOKINGS']  # Master bookings has 9958 rows as of 12-15-18
     path_to_files = home + '\\' + working_dir + '\\'
     path_to_bookings = path_to_files + bookings_file
 
@@ -18,7 +18,6 @@ def build_customer_list():
     sheet_bookings = wb_bookings.sheet_by_index(0)
 
     bookings_dict = build_bookings_dict(path_to_bookings, sheet_bookings)
-
     customer_list = []
     col_num_end = 0
     col_num_erp = 0
@@ -45,7 +44,6 @@ def build_customer_list():
         customer_name_end = sheet_bookings.cell_value(i, col_num_end)
         customer_list.append((customer_name_erp, customer_name_end))
 
-
     # Create a simple customer_list list of tuples
     # Contains a full set of unique sorted customer names
     # customer_list = [(erp_customer_name,end_customer_ultimate), (CustA,CustA)]
@@ -61,10 +59,10 @@ def build_customer_list():
     # Write TA Customer List to a local excel workbook
     #
     # Insert a header row before writing
-    top_row = ['erp_customer_name','end_customer_ultimate_name']
-    customer_list.insert(0,top_row)
+    top_row = ['erp_customer_name', 'end_customer_ultimate_name']
+    customer_list.insert(0, top_row)
 
-    wb_file = path_to_files + app['XLS_CUSTOMER'] + app['AS_OF_DATE']  + '.xlsx'
+    wb_file = path_to_files + app['XLS_CUSTOMER'] + app['AS_OF_DATE'] + '.xlsx'
     workbook = xlsxwriter.Workbook(wb_file)
     worksheet = workbook.add_worksheet()
     for this_row, my_val in enumerate(customer_list):
@@ -76,8 +74,5 @@ def build_customer_list():
 
 
 if __name__ == "__main__":
-    print(build_customer_list())
     our_customers = build_customer_list()
     print('We have: ', len(our_customers), ' customers')
-
-
